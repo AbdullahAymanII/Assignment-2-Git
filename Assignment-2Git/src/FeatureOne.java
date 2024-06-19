b14cddc8cdfeb64b7d90f0f7a50149dcaaae407f
abd1 <abd1@gmail.com>
2024-06-19 12:35:22 +0300

    add new feature-1 print array of numbers

---
+++ Assignment-2Git/src/FeatureOne.java
@@ -0,0 +1,7 @@
+public class FeatureOne {
+    public static void printIntigers(int[]arr){
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
