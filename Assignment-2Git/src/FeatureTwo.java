c25d7a79f20d01a9baf120aed51a27ce4c21c53f
AbdullahAymanII <abdayman1092002@gmail.com>
2024-06-19 12:40:16 +0300

    add new feature-2 print array of doubles

---
+++ Assignment-2Git/src/FeatureTwo.java
@@ -0,0 +1,7 @@
+public class FeatureTwo {
+    public static void printDoubles(double[]arr){
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
